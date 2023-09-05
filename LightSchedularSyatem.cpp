b26db73e0dff9f003072f641a932c8360ec6960c
Venugopal K <venu.shastri@gmail.com>
2023-09-05 14:59:38 +0530

    Update LightSchedularSyatem.cpp

--- LightSchedularSyatem.cpp
+++ LightSchedularSyatem.cpp
@@ -40,12 +40,14 @@ class TimerService:public ITimerService
  private:
 	IOpertingSystemService* rtosObj;
 public:
-	string getTime() { return ""; }
+	string getTime() { rtosObj->updateTime(); return ""; }
+	TimerService(IOpertingSystemService* rtosArg):rtosObj(rtosArg){}
 };
 class LightController:public ILightController{
 private:
 	ILightHardware* hardwareObj;
 public:
+	LightController(ILightHardware* hwArg): hardwareObj(hwArg){}
 	void on(){}
 	void off() {}
 };
@@ -55,9 +57,79 @@ private:
 	ITimerService* _timerObj;
 	ILightController* _controllerObj;
 public:
-	 void removeSchdule(){}
-	 void wakeup(){}
-	 void turnSchedularOn() {}
+	LightSchedular(ITimerService* timerArg,ILightController* controllerArg):
+		_timerObj(timerArg), _controllerObj(controllerArg){}
+	 void removeSchdule(){
+		 this->_controllerObj->off();
+	 }
+	 void wakeup(){
+		 this->_timerObj->getTime();
+	 }
+	 void turnSchedularOn() {
+		 this->_controllerObj->on();
+	 }
+
 
 };
 
+/*
+class Vehicle{
+
+};
+class StartupMotor {};
+class FuelPump{};
+class Engine {
+private :
+	FuelPump fuelPumpObj;
+	StartupMotor motorObj;
+public:
+	void crankEngine(){
+	   //Delegate -interact 
+	}
+};
+//parent-child (iheritance)
+class Car :public Vehicle {
+	Engine engObj;
+public:
+	void start() {
+		engObj.crankEngine();
+	}
+
+};
+
+
+class Engine {};
+class IVehicle {
+public:
+	virtual void drive() = 0;
+};
+class Car:public IVehicle{
+	Engine* enginePtr;
+public:
+	//Constructor Engine
+	Car(Engine* engineArg): enginePtr(engineArg) {
+
+	 }
+	void drive() {
+
+	}
+
+};
+class Person {
+public:
+	//Dependency Injection (Method Injection)
+	void commute(IVehicle* vehiclePtr) {
+		 //start using car
+		vehiclePtr->drive();
+	}
+
+};
+
+int main() {
+	Engine engObj;
+	Car carObj(&engObj);
+	Person personObj;
+	personObj.commute(&carObj);
+
+}
+*/
